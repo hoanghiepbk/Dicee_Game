d8d6e872aacbd832719ba23f0f193d2c2d41945e
hoanghiep <hoanghiepphambk@gmail.com>
2022-01-06 21:14:36 +0700

    commit tom tit

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+document.getElementById("myBtn").addEventListener("click", play);
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var scoreTit = 0;
+var scoreTom = 0;
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+function play() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var image1 = document.querySelectorAll("img")[0];
+  var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-image1.setAttribute("src", randomImageSource);
+  var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
+  var image1 = document.querySelectorAll("img")[0];
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+  image1.setAttribute("src", randomImageSource);
 
 
-//If player 1 wins
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
-}
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-}
-else {
-  document.querySelector("h1").innerHTML = "Draw!";
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+  //If player 1 wins
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Tit Wins!";
+    scoreTit++;
+    document.querySelectorAll("span")[0].textContent = " (" + scoreTit + ")"
+  }
+  else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Tom Wins! 🚩";
+    scoreTom++;
+    document.querySelectorAll("span")[1].textContent = " (" + scoreTom + ")"
+  }
+  else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
 }
